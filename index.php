90ebb91f5f58d5193313857f9aa089122a521167
Susmita Debnath <supti.debnath@gmail.com>
2018-05-10 19:33:42 +0600

    fixing sign up design.

--- index.php
+++ index.php
@@ -49,7 +49,8 @@
 <html>
 <head>
 	<title>Funbook.com</title>
-	<link rel="stylesheet" type="text/css" href="css/design.css">
+	<link rel="stylesheet" type="text/css" href="css/design.css?ver=<?php echo time() ?>">
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
 </head>
 <body>
 	<div id="wrapper">
@@ -58,6 +59,90 @@
 				<div id="writing">
 					funbook
 				</div>
+				<div id="box">
+					<form method="post">
+						<div id="email">
+							<label for="email">Email</label>
+							<input type="text" name="email"><br>
+						</div>
+						<div id="space">
+							
+						</div>
+						<div id="password">
+							<label for="password">Password</label>
+							<input type="password" name="password"><br>
+						</div>
+						<div id="button">
+							<input type="submit" id="login_btn" name="login" value="Log In">							
+						</div>
+						<div class="clr"></div>
+					</form>
+				</div>
+				<div class="clr"></div>
+			</div>
+		</div>
+
+		<div id="full">
+			<div id="information">
+				<div id="none">
+					<h2>Funbook helps you connect and share with the people in your life.</h2>
+				</div>
+				<div id="register">
+					<h2>Create a new account</h2><br>
+					<h4>It's free and always will be.</h4><br>
+
+					<?php if ( isset( $valid ) && ! $valid ) { ?>
+						<div class="error">
+							<ul>
+								<?php foreach ($msg as $value) { ?> 
+									<li><?php echo $value; ?></li>  
+								<?php } ?>
+							</ul>
+						</div>
+					<?php } ?>
+
+					<form method="post">
+						<b>Email</b><br><input type="text" name="email"><br>
+						<b>Username</b><br><input type="text" name="username"><br>
+						<b>Password</b><br><input type="password" name="password"><br>
+						<b>Retype Password</b><br><input type="password" name="retype"><br><br>
+						<input type="submit" name="register" value="Sign Up">
+					</form>
+				</div>
+				<div class="clr"></div>
+			</div>
+		</div>
+		
+
+		<div id="admit">
+			<div id="register" width="50%">
+			<h2>Register</h2><br>
+
+				<?php if ( isset( $valid ) && ! $valid ) { ?>
+					<div class="error">
+						<ul>
+							<?php foreach ($msg as $value) { ?> 
+								<li><?php echo $value; ?></li>  
+							<?php } ?>
+						</ul>
+					</div>
+				<?php } ?>
+
+				<form method="post">
+					<b>Email</b><br><input type="text" name="email"><br>
+					<b>Username</b><br><input type="text" name="username"><br>
+					<b>Password</b><br><input type="password" name="password"><br>
+					<b>Retype Password</b><br><input type="password" name="retype"><br><br>
+					<input type="submit" name="register" value="Register">
+				</form>
+			</div>
+			<div id="login" width="50%">
+			<h2>Log in</h2><br>
+				<form method="post">
+					<b>Username</b><br><input type="text" name="username"><br>
+					<b>Password</b><br><input type="password" name="password"><br><br>
+					<input type="submit" name="login" value="Log in">
+				</form>
 			</div>
 		</div>
 
@@ -97,8 +182,6 @@
 
 
 
-
-		<h1>Welcome to Funbook.com Hello</h1><br>
 		<div id="register" width="50%">
 			<h2>Register</h2><br>
 
